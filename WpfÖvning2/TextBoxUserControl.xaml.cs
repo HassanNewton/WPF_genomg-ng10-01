1eda66b33435a6538c8008686dceb5f4c4794e9a
HassanNewton <hassan.hussin@newton.se>
2024-10-01 14:28:49 +0200

    New changes

--- WpfÖvning2/TextBoxUserControl.xaml.cs
+++ WpfÖvning2/TextBoxUserControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfÖvning2
 {
@@ -23,6 +11,24 @@ namespace WpfÖvning2
         public TextBoxUserControl()
         {
             InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Clear();
+            txtBox.Focus();
+        }
+
+        private void txtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBox.Text))
+            {
+                tbPlaceHolder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceHolder.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
